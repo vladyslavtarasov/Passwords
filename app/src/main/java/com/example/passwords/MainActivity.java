7351a020a3a062e3512e7575858acb9d4f873e3f
Vladyslav Tarasov <vladyslavtarasov02@gmail.com>
2023-05-08 22:23:25 +0100

    Add button methods for show, hide, and view password

--- app/src/main/java/com/example/passwords/MainActivity.java
+++ app/src/main/java/com/example/passwords/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.passwords;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showPassword(View view) {
+        EditText password = findViewById(R.id.password_text);
+        password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+    }
+
+    public void hidePassword(View view) {
+        EditText password = findViewById(R.id.password_text);
+        password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+    }
+
+    public void viewPassword(View view) {
+        EditText password = findViewById(R.id.password_text);
+        TextView passwordView = findViewById(R.id.password);
+        passwordView.setText("Your password: " + password.getText());
+        passwordView.setVisibility(View.VISIBLE);
+        password.setText("");
     }
 }
