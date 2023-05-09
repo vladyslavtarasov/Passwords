d9b4fc15274486da629c9cda6b888632a54a2ad1
Vladyslav Tarasov <vladyslavtarasov02@gmail.com>
2023-05-09 22:40:18 +0100

    Disable buttons when they are not needed

--- app/src/main/java/com/example/passwords/MainActivity.java
+++ app/src/main/java/com/example/passwords/MainActivity.java
@@ -6,28 +6,41 @@ import android.os.Bundle;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button hideButton, showButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        hideButton = findViewById(R.id.hidePassword_button);
+        showButton = findViewById(R.id.showPassword_button);
+        hideButton.setEnabled(false);
     }
 
     public void showPassword(View view) {
         EditText password = findViewById(R.id.password_text);
+        //if (!password.getText().toString().equals("")) {
         password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
         password.setSelection(password.length());
+        showButton.setEnabled(false);
+        hideButton.setEnabled(true);
+        //}
     }
 
     public void hidePassword(View view) {
         EditText password = findViewById(R.id.password_text);
+        //if (!password.getText().toString().equals("")) {
         password.setTransformationMethod(PasswordTransformationMethod.getInstance());
         password.setSelection(password.length());
+        showButton.setEnabled(true);
+        hideButton.setEnabled(false);
+        //}
     }
 
     public void viewPassword(View view) {
