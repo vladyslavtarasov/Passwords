0aeb0b9c88a7eccbd042b72003b5417160a3b439
Vladyslav Tarasov <vladyslavtarasov02@gmail.com>
2023-05-09 23:26:56 +0100

    Start adding fragments

---
+++ app/src/main/java/com/example/passwords/fragments/EnterFragment.java
@@ -0,0 +1,11 @@
+package com.example.passwords.fragments;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.passwords.R;
+
+public class EnterFragment extends Fragment {
+    public EnterFragment() {
+        super(R.layout.fragment_enter);
+    }
+}
---
+++ app/src/main/java/com/example/passwords/fragments/ShowFragment.java
@@ -0,0 +1,11 @@
+package com.example.passwords.fragments;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.passwords.R;
+
+public class ShowFragment extends Fragment {
+    public ShowFragment() {
+        super(R.layout.fragment_show);
+    }
+}
